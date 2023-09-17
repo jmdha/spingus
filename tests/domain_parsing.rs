31bceec2d904f79ed7cbd48bd35f28b92e9addc7
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2023-09-17 19:26:15 +0200

    init

---
+++ tests/domain_parsing.rs
@@ -0,0 +1,36 @@
+use std::{env, fs};
+
+use spingus::domain;
+
+use rstest::*;
+
+#[rstest]
+#[case("gripper")]
+#[case("logistics")]
+#[case("movie")]
+#[case("mystery")]
+#[case("blocks-typed")]
+#[case("blocks-untyped")]
+#[case("elevator-typed")]
+#[case("elevator-untyped")]
+#[case("freecell-typed")]
+#[case("freecell-untyped")]
+#[case("logistics-typed")]
+#[case("logistics-untyped")]
+fn parse_domain(#[case] domain_name: &str) {
+    let path = env::current_dir().unwrap();
+    println!("The current directory is {}", path.display());
+    if let Ok(str) = fs::read_to_string(format!("tests/data/{}/domain.pddl", domain_name)) {
+        let parse_result = domain::parse_domain(&str);
+        if let Ok(dom) = parse_result {
+            assert!(!dom.name.is_empty());
+        } else if let Err(err) = parse_result {
+            panic!(
+                "Could not parse domain: \"{}\".\nWith error: \"{}\"",
+                domain_name, err
+            );
+        }
+    } else {
+        panic!("Could not open");
+    }
+}
